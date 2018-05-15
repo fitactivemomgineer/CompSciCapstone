9c4259d455793dd3afddeaf6324a1feb040355d4
Christine Hodges <christine.hodges@smail.rasmussen.edu>
2018-05-15 12:37:06 -0500

    Adding More pages
    This includes the 4 other pages and some styling until those pages are built

--- welcome.php
+++ welcome.php
@@ -22,7 +22,36 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 <body>
     <div class="page-header">
         <img src="logo.JPG" alt= "female running">
-        <h1> Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome to our site.</h1>
+        <h1> Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>! <br> Welcome to The Ultimate Marathon Mom.</h1>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <ul class="nav nav-tabs">
+      <li>
+        <a href="runners.html">Runners</a>
+      </li>
+      <li>
+        <a href="bikers.html">Bikers</a>
+      </li>
+      <li>
+        <a href="apparel.html">Apparel</a>
+      </li>
+      <li>
+        <a href="nutrition.html">Nutrition</a>
+      </li>
+    </ul>
+    <div class="jumbotron">
+      <h2>
+        Meet Christine, our TUMM community leader!
+      </h2>
+      <p>
+         She has created this website for momma's who are lovers of fitness, health, and fashion!
+        <br>
+         Because let's face it, things just don't move the same after having a baby!
+      <p>
+        <a class="btn btn-primary btn-large" href="#">Learn more</a>
+      </p>
     </div>
     <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
 </body>
